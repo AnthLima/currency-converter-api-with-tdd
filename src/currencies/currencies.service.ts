881b7dcbaacf1adee230ab5c300f32813ed37c0a
AnthLima <tryout3699@gmail.com>
2023-02-27 00:17:20 -0300

    [feat][main]: create method updateCurrency in currencies service

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,15 +1,60 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
 export class CurrenciesRepository {
   async getCurrency(currency: string): Promise<any> {
-    return {};
+    //
+  }
+
+  async createCurrency({ currency, value }): Promise<any> {
+    //
+  }
+
+  async updateCurrency({ currency, value }): Promise<any> {
+    //
   }
 }
+
 @Injectable()
 export class CurrenciesService {
   constructor(private currenciesRepository: CurrenciesRepository) {}
+
   async getCurrency(currency: string): Promise<any> {
     try {
       return await this.currenciesRepository.getCurrency(currency);
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async createCurrency({ currency, value }): Promise<any> {
+    if (value <= 0) {
+      throw new BadRequestException(`The value must be greater zero.`);
+    }
+
+    try {
+      return await this.currenciesRepository.createCurrency({
+        currency,
+        value,
+      });
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async updateCurrency({ currency, value }): Promise<any> {
+    if (value <= 0) {
+      throw new BadRequestException(`The value must be greater zero.`);
+    }
+
+    try {
+      return await this.currenciesRepository.updateCurrency({
+        currency,
+        value,
+      });
     } catch (error) {
       throw new InternalServerErrorException();
     }
