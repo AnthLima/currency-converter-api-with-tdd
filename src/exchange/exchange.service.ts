1c6331b0e24f4e9eed41495463753be3bf785782
AnthLima <tryout3699@gmail.com>
2023-02-26 21:45:34 -0300

    [refactor][main]: Refactoring tests, adding types and refactoring converAmount in exchange service

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,11 +1,17 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { ExchangeInputType } from './types/exchange.inputs.type';
+import { ExchangeType } from './types/exchange.type';
 export class CurrenciesService {
   async getCurrency(currency: string): Promise<any> {}
 }
 @Injectable()
 export class ExchangeService {
   constructor(private currenciesService: CurrenciesService) {}
-  async convertAmount({ from, to, amount }): Promise<any> {
+  async convertAmount({
+    from,
+    to,
+    amount,
+  }: ExchangeInputType): Promise<ExchangeType> {
     if (!from || !to || !amount) {
       throw new BadRequestException();
     }
