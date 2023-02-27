81a9811a17e2923e5aa7d037fde419b37ab1a327
AnthLima <tryout3699@gmail.com>
2023-02-26 23:34:13 -0300

    [feat][main]: ensure getCurrency throw if repository throw and not throw if repository returns

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class CurrenciesService {
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+export class CurrenciesRepository {
   async getCurrency(currency: string): Promise<any> {
     //
+  }
+}
+@Injectable()
+export class CurrenciesService {
+  constructor(private currenciesRepository: CurrenciesRepository) {}
+  async getCurrency(currency: string): Promise<any> {
+    try {
+      await this.currenciesRepository.getCurrency(currency);
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
   }
 }
